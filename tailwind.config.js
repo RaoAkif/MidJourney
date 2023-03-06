19e916cf6e6c1ae20b4e8a6eac26ae50c11d4468
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-03-06 22:41:53 +0500

    Complete RN @Tailwind Configuration

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./app/**/*.{html,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: require('tailwind-rn/unsupported-core-plugins'),
+}
