587c3786bd49f76ae2e279286de5d5b88b6b892c
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-03-28 23:53:52 +0500

    Add read page

--- app/dashboard/read.jsx
+++ app/dashboard/read.jsx
@@ -1,15 +1,13 @@
-import { useRouter } from "expo-router";
+import { Link } from "expo-router";
 import React from "react";
 import { Button } from "react-native";
-import { StyleSheet, View, Image, Text } from "react-native";
+import { StyleSheet, View, Image, Text, TextInput } from "react-native";
 
 const bgColor = "#fefbf6";
 const bgWhite = "#ffffff";
 const black = "#000000";
-const hatbgColor = "#bcbcbc";
-const borderLine = "#d2d2d2";
-const profileText = "#333";
-const editText = "#979797";
+const buttonbgColor = "#e4504d";
+const borderColor = "#bcbcbc";
 
 const styles = StyleSheet.create({
   container: {
@@ -32,36 +30,24 @@ const styles = StyleSheet.create({
     width: 80,
     height: 80,
   },
-  profileContainer: {
+  goBack: {
+    width: 20,
+    height: 20,
+    position: "absolute",
+    left: "10px",
+    top: "25px",
+  },
+  storiesContainer: {
     flex: 1,
     width: "100%",
     justifyContent: "center",
     alignItems: "center",
   },
-  hatContainer: {
-    width: 60,
-    height: 60,
-    borderRadius: "50%",
-    borderColor: hatbgColor,
-    borderWidth: 1,
-    backgroundColor: bgColor,
-    justifyContent: "center",
-    alignItems: "center",
-    elevation: 2,
-    margin: -45,
-    zIndex: 1,
-    marginLeft: "-80%",
-  },
-  hat: {
-    width: 52,
-    height: 52,
-  },
-  contentContainer: {
+  storyContainer: {
     // flex: 1,
     justifyContent: "end",
     alignItems: "baseline",
-    width: "70%",
-    height: "50%",
+    width: "80%",
     backgroundColor: bgWhite,
     // elevation
     shadowColor: black,
@@ -73,58 +59,80 @@ const styles = StyleSheet.create({
     shadowRadius: 4.65,
     elevation: 7,
   },
-  profileName: {
-    padding: 10,
-    textAlign: "center",
-    color: profileText,
-    fontWeight: 700,
-  },
-  storiesContainer: {
+  storyTextContainer: {
     flex: 1,
     width: "100%",
-    justifyContent: "space-between",
     alignItems: "center",
+    paddingTop: "15px",
+    paddingBottom: "5px",
   },
-  storyItem: { flexDirection: "row", alignItems: "center" },
-  count: {
-    padding: 10,
-    textAlign: "center",
-    color: black,
-    fontWeight: 700,
-    fontSize: 26,
+  storyTitle: {
+    fontSize: "16px",
+    color: "#333332",
+    fontWeight: "bold",
   },
-  storyMessage: {
-    padding: 10,
-    textAlign: "center",
-    color: black,
-    fontWeight: 700,
+  storyDescription: {
+    fontSize: "16px",
+    color: "#333332",
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    paddingHorizontal: "10px",
+    textAlign: "justify",
   },
-  editText: {
-    padding: 10,
-    textAlign: "center",
-    color: editText,
-    fontWeight: 700,
-    fontSize: 12,
+  downArray: {
+    marginTop: "10px",
   },
-  options: {
-    borderWidth: 1,
-    borderColor: borderLine,
-    backgroundColor: bgWhite,
-    height: 62,
+  collaborationContainerInput: {
     width: "100%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
+    height: "100%",
   },
-  optionsImage: {
-    width: 45,
-    height: 45,
+  collaborationInput: {
+    fontSize: "16px",
+    color: "#333332",
+    textAlign: "center",
+    paddingVertical: "10%",
+  },
+  button: {
+    width: "80vw",
+    height: 46,
+    backgroundColor: buttonbgColor,
+    textAlign: "center",
+    justifyContent: "center",
+    // elevation
+    shadowColor: black,
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+  },
+  buttonText: {
+    color: bgWhite,
+    fontSize: "16px",
+  },
+  hatsList: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20
+  },
+  hatContainer: {
+    padding: "15px",
+  },
+  hat: {
+    width: 40,
+    height: 40,
+    borderColor,
+    borderWidth: 1,
+    borderRadius: "50%",
   },
 });
 
 export default function Profile() {
-  const name = "Umman";
-  const router = useRouter();
   return (
     <View style={styles.container}>
       <View style={styles.topHatContainer}>
@@ -133,31 +141,43 @@ export default function Profile() {
           source={require("../../assets/thinking_cap1.png")}
         />
       </View>
-      <View style={styles.profileContainer}>
-        <View style={styles.hatContainer}>
-          <Image
-            style={styles.hat}
-            source={require("../../assets/thinking_cap4_tilted.png")}
-          />
+      <View style={styles.goBack}>
+        <Image
+          style={styles.goBackIcon}
+          source={require("../../assets/left1.png")}
+        />
+      </View>
+      <View style={styles.storiesContainer}>
+        <View style={styles.storyContainer}>
+          <View style={styles.storyTextContainer}>
+            <Text style={styles.storyTitle}>Sleep Hollow</Text>
+            <Text style={styles.storyDescription}>No other sounds were ever heard from the basement anymore. They always wondered if the ghost had finally  dissappeared from the house. No other sounds were ever heard from the dusty basement anymore. They always would wonder if the ghost had finally left and dissappeared from the house.They always  wondered if the ghost had finally  dissappeared from the house. They always  wondered if the ghost had finally  dissappeared from the house. I They always  wondered if the ghost had finally  dissappeared from the house</Text>
+          </View>
         </View>
-        <View style={styles.contentContainer}>
-          <View style={styles.storiesContainer}>
-            <View>
-              <Text style={styles.profileName}>John Rabada</Text>
-            </View>
-            <View>
-              <View style={styles.storyItem}>
-                <Text style={styles.count}>33</Text>
-                <Text style={styles.storyMessage}>Stories Created</Text>
-              </View>
-              <View style={styles.storyItem}>
-                <Text style={styles.count}>300</Text>
-                <Text style={styles.storyMessage}>Stories Collaborated</Text>
-              </View>
-            </View>
-            <View>
-              <Text style={styles.editText}>edit</Text>
-            </View>
+        <View style={styles.hatsList}>
+          <View style={styles.hatContainer}>
+            <Image
+              style={styles.hat}
+              source={require("../../assets/thinking_cap1.png")}
+            />
+          </View>
+          <View style={styles.hatContainer}>
+            <Image
+              style={styles.hat}
+              source={require("../../assets/thinking_cap2.png")}
+            />
+          </View>
+          <View style={styles.hatContainer}>
+            <Image
+              style={styles.hat}
+              source={require("../../assets/thinking_cap3.png")}
+            />
+          </View>
+          <View style={styles.hatContainer}>
+            <Image
+              style={styles.hat}
+              source={require("../../assets/thinking_cap4.png")}
+            />
           </View>
         </View>
       </View>
