d5b4d6d251d18389f8fe3c89d4003b218d31071d
Umman <ummanw@yahoo.com>
2023-05-05 08:22:43 +0500

    update bottom tab icons

--- app/home/_layout.jsx
+++ app/home/_layout.jsx
@@ -1,5 +1,11 @@
 import { Tabs } from "expo-router";
 import { Image, StyleSheet } from "react-native";
+import Write1 from "../../assets/write1.png";
+import Write2 from "../../assets/write2.png";
+import Profile1 from "../../assets/profile1.png";
+import Profile2 from "../../assets/profile2.png";
+import Read1 from "../../assets/read1.png";
+import Read2 from "../../assets/read2.png";
 
 const styles = StyleSheet.create({
   optionsImage: {
@@ -23,7 +29,7 @@ export default function Layout() {
       screenOptions={{
         headerShown: false,
         tabBarShowLabel: false,
-        tabBarStyle: styles.tabBar
+        tabBarStyle: styles.tabBar,
       }}
     >
       <Tabs.Screen
@@ -33,61 +39,46 @@ export default function Layout() {
         }}
         options={{
           tabBarItemStyle: styles.tabBarOption,
-          tabBarIcon: () => (
-            <Image
-              style={styles.optionsImage}
-              source={require("../../assets/write1.png")}
-            />
-          ),
+          tabBarIcon: ({ focused }) => <Image style={styles.optionsImage} source={focused ? Write2 : Write1} />,
         }}
-        name='create'
+        name="create"
       />
       <Tabs.Screen
         options={{
           tabBarItemStyle: styles.tabBarOption,
-          tabBarIcon: () => (
-            <Image
-              style={styles.optionsImage}
-              source={require("../../assets/read1.png")}
-            />
-          ),
+          tabBarIcon: ({ focused }) => <Image style={styles.optionsImage} source={focused ? Read2 : Read1} />,
         }}
-        name='collaborations'
+        name="collaborations"
       />
       <Tabs.Screen
         options={{
-          tabBarIcon: () => (
-            <Image
-              style={styles.optionsImage}
-              source={require("../../assets/profile2.png")}
-            />
-          ),
+          tabBarIcon: ({ focused }) => <Image style={styles.optionsImage} source={focused ? Profile2 : Profile1} />,
         }}
-        name='index'
+        name="index"
       />
       <Tabs.Screen
         options={{
           href: null,
         }}
-        name='read'
+        name="read"
       />
       <Tabs.Screen
         options={{
           href: null,
         }}
-        name='myStories'
+        name="myStories"
       />
       <Tabs.Screen
         options={{
           href: null,
         }}
-        name='myCollaborations'
+        name="myCollaborations"
       />
       <Tabs.Screen
         options={{
           href: null,
         }}
-        name='collaborate'
+        name="collaborate"
       />
     </Tabs>
   );
