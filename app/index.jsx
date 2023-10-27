a482bbc019b321792eb645c0828c503b04bb7ede
Umman <ummanw@yahoo.com>
2023-10-27 16:14:07 +0500

    login screen updated

--- app/index.jsx
+++ app/index.jsx
@@ -4,9 +4,10 @@ import { useSelector, useDispatch } from "react-redux";
 import { StyleSheet, Text, TextInput, View, Image, Pressable } from "react-native";
 import { useLoginMutation } from "../redux/api/authApi";
 import { login } from "../redux/slices/authSlice";
-import { useNavigation } from "expo-router";
+import { Link, useNavigation } from "expo-router";
 import { COLORS } from "../utils/constants";
 import Container from "../components/ui/Container";
+import tw from "../utils/tailwind";
 
 export default function Landing() {
   const pseudonymRef = useRef(null);
@@ -82,15 +83,17 @@ export default function Landing() {
 
   return (
     <Container>
-      <View style={styles.contentContainer}>
-        <View style={styles.signupContainer}>
-          <View style={styles.hatContainer}>
+      <View style={tw`w-full items-center flex-1 justify-center px-8`}>
+        <View style={tw`w-full items-center`}>
+          <View
+            style={tw`elevation z-10 bg-[${COLORS.bgColor}] w-20 h-20 rounded-full justify-center items-center -mb-10 `}
+          >
             <Image
-              style={styles.hat}
+              style={tw`w-14 h-14 p-3`}
               source={{ uri: "https://raw.githubusercontent.com/Immages/writinghat/main/caps/thinking_cap1.png" }}
             />
           </View>
-          <View style={styles.inputContainer}>
+          <View style={tw`h-52 bg-[${COLORS.bgWhite}] w-full elevation justify-center items-center mb-3 px-6`}>
             <TextInput
               ref={pseudonymRef}
               style={styles.input}
@@ -125,11 +128,14 @@ export default function Landing() {
           </View>
         </View>
         {errorMessage && <Text style={styles.credentialsErrorText}>{errorMessage}</Text>}
-        <View style={styles.button}>
+        <View style={tw`elevation w-full h-12 bg-[${COLORS.buttonbgColor}] items-center justify-center`}>
           <Pressable onPress={handleOnPress}>
-            <Text style={styles.buttonText}>Start Writing</Text>
+            <Text style={tw`text-base text-[${COLORS.bgWhite}]`}>Start Writing</Text>
           </Pressable>
         </View>
+        <Link style={tw`mt-5 text-[#877965] font-medium text-base`} href={"/register"}>
+          <Text>Register</Text>
+        </Link>
       </View>
       <Text style={styles.messageText}>
         &quot;If you can tell stories, create characters, devise incidents, and have sincerity and passion, it
@@ -140,34 +146,8 @@ export default function Landing() {
 }
 
 const styles = StyleSheet.create({
-  contentContainer: {
-    justifyContent: "flex-end",
-    alignItems: "center",
-    width: "100%",
-  },
-  signupContainer: {
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  inputContainer: {
-    width: "80%",
-    height: 200,
-    backgroundColor: COLORS.bgWhite,
-    // elevation
-    shadowColor: COLORS.black,
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.29,
-    shadowRadius: 4.65,
-    elevation: 7,
-    marginBottom: 12,
-    justifyContent: "center",
-    alignItems: "center",
-  },
   input: {
+    width: "100%",
     borderBottomWidth: 1,
     borderBottomColor: COLORS.borderbgColor,
     textAlign: "center",
@@ -194,53 +174,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     width: "80%",
   },
-  button: {
-    width: "70%",
-    height: 46,
-    backgroundColor: COLORS.buttonbgColor,
-    textAlign: "center",
-    justifyContent: "center",
-    // elevation
-    shadowColor: COLORS.black,
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.29,
-    shadowRadius: 4.65,
-    elevation: 7,
-  },
-  buttonText: {
-    color: COLORS.bgWhite,
-    fontSize: 16,
-  },
-  hatContainer: {
-    backgroundColor: COLORS.bgColor,
-    width: 90,
-    height: 82,
-    borderRadius: 50,
-    borderColor: COLORS.hatbgColor,
-    borderWidth: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    // elevation
-    shadowColor: "#D9D9D9",
-    opacity: 1,
-    shadowOffset: {
-      width: 0,
-      height: -3,
-    },
-    shadowOpacity: 0.7,
-    shadowRadius: 1,
-    elevation: 7,
-    marginBottom: -41,
-    zIndex: 1,
-  },
-  hat: {
-    width: 55,
-    height: 55,
-    padding: 10,
-  },
+
   messageText: {
     padding: 10,
     textAlign: "center",
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
+const plugin = require("tailwindcss/plugin");
+const { COLORS } = require("./utils/constants");
 module.exports = {
   content: ["./app/**/*.{html,js,jsx}", "./components/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        ".elevation": {
+          shadowColor: COLORS.black,
+          shadowOffset: {
+            width: 0,
+            height: 3,
+          },
+          shadowOpacity: 0.29,
+          shadowRadius: 4.65,
+          elevation: 7,
+        },
+      });
+    }),
+  ],
 };
